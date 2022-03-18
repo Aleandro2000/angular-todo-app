5c0ee6925a183102a9c732de70da1d3ec2fb959e
Alexandru Carmici <66217150+Aleandro2000@users.noreply.github.com>
2022-03-18 13:06:10 +0200

    angular-project

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -13,18 +13,25 @@ export class DashboardComponent implements OnInit {
   constructor(
     private apiService: ApiService
   ) { }
-  
+
   users: any;
+
   searchDataForm = new FormGroup({
     search: new FormControl()
   });
+
+  name = new FormControl();
+  email = new FormControl();
+  street = new FormControl();
+  city = new FormControl();
+  zip = new FormControl();
 
   ngOnInit(): void {
     this.getAllUsers();
   }
 
   onSubmit() {
-      this.searchDataForm.value.search ? this.users = this.users.filter((element: any) => element.email.includes(this.searchDataForm.value.search)) : this.getAllUsers();
+    this.searchDataForm.value.search ? this.users = this.users.filter((element: any) => element.email.includes(this.searchDataForm.value.search)) : this.getAllUsers();
   }
 
   getAllUsers() {
@@ -39,20 +46,31 @@ export class DashboardComponent implements OnInit {
       );
   }
 
-  editUser(data: any) {
-    this.apiService.editUser(data)
-      .subscribe(
-        data => {},
-        error => {
-          alert(error.message);
-        }
-      );
+  editUser(_id: any) {
+    alert(this.name.value)
+    if (this.name.value && this.email.value && this.city.value && this.street.value && this.zip.value)
+      this.apiService.editUser({
+        _id,
+        name: this.name.value,
+        email: this.email.value,
+        city: this.city.value,
+        street: this.street.value,
+        zip: this.zip.value
+      })
+        .subscribe(
+          data => { },
+          error => {
+            alert(error.message);
+          }
+        );
+      else
+        alert("Sorry! The form may not update the user!");
   }
 
-  deleteUser(data: any) {
-    this.apiService.deleteUser(data)
+  deleteUser(_id: any) {
+    this.apiService.deleteUser({ _id })
       .subscribe(
-        data => {},
+        data => { },
         error => {
           alert(error.message);
         }
