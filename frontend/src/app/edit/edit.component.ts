4901a720bb4e99076cc563559f3b0248d9923245
Alexandru Carmici <66217150+Aleandro2000@users.noreply.github.com>
2022-03-18 18:12:50 +0200

    angular-project

--- frontend/src/app/edit/edit.component.ts
+++ frontend/src/app/edit/edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-edit',
@@ -9,11 +11,34 @@ import { Router } from '@angular/router';
 export class EditComponent implements OnInit {
 
   constructor(
+    private apiService: ApiService,
     private router: Router
   ) { }
 
   ngOnInit() {
     !sessionStorage.getItem("id") ? this.router.navigate(["/"]) : null;
+  }
+
+  users: any;
+
+  userDataForm = new FormGroup({
+    name: new FormControl(),
+    email: new FormControl(),
+    street: new FormControl(),
+    city: new FormControl(),
+    zip: new FormControl()
+  });
+
+  onUserSubmit() {
+    this.apiService.editUser({...this.userDataForm.value, id: sessionStorage.getItem("id")})
+      .subscribe(
+        data => {
+          this.router.navigate(["/"]);
+        },
+        error => {
+          alert(error.message);
+        }
+      );
   }
 
 }
